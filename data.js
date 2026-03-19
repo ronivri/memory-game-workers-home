f2cd2c93809a731245549bb95ca0a0bb84902fc4
ronivri <ronivri@gmail.com>
2026-03-19 14:20:58 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,66 @@
+// data.js — מודל A: התאמת עובד לתמונת יישוב
+const EMPLOYEES = [
+  {
+    "employeeId": 1,
+    "name": "דרור כרמלי",
+    "locationId": 1,
+    "locationName": "תמרת",
+    "imageExt": "png"
+  },
+  {
+    "employeeId": 2,
+    "name": "לילך ואנונו",
+    "locationId": 2,
+    "locationName": "כרמיאל",
+    "imageExt": "png"
+  },
+  {
+    "employeeId": 3,
+    "name": "עודד נריה",
+    "locationId": 5,
+    "locationName": "טבעון",
+    "imageExt": "jfif"
+  },
+  {
+    "employeeId": 4,
+    "name": "ענת לנדאו",
+    "locationId": 6,
+    "locationName": "כרמיאל",
+    "imageExt": "png"
+  },
+  {
+    "employeeId": 5,
+    "name": "ארז פיקסל",
+    "locationId": 7,
+    "locationName": "גניגר",
+    "imageExt": "jfif"
+  },
+  {
+    "employeeId": 6,
+    "name": "יוסי פרי-דן",
+    "locationId": 9,
+    "locationName": "יובלים",
+    "imageExt": "png"
+  },
+  {
+    "employeeId": 7,
+    "name": "רון עברי",
+    "locationId": 10,
+    "locationName": "שמשית",
+    "imageExt": "jfif"
+  },
+  {
+    "employeeId": 8,
+    "name": "אריק מימרן",
+    "locationId": 11,
+    "locationName": "חיפה",
+    "imageExt": "png"
+  },
+  {
+    "employeeId": 9,
+    "name": "אביב נווה",
+    "locationId": 23,
+    "locationName": "פרדס חנה",
+    "imageExt": "png"
+  }
+] ;
